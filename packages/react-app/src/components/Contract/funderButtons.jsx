090715722000a31ba555967504367ef332f49f52
andrewhong5297 <47720952+andrewhong5297@users.noreply.github.com>
2020-10-17 02:58:15 -0400

    changed button to have faucet, as well as web3 connection

--- packages/react-app/src/components/Contract/funderButtons.jsx
+++ packages/react-app/src/components/Contract/funderButtons.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
+import { ethers } from "ethers";
 
 export const Buttons = (props) => {
   const { register, handleSubmit } = useForm();
@@ -8,27 +9,47 @@ export const Buttons = (props) => {
     "haven't called yet, click call button",
   ]);
 
-  const callAPI = () => {
-    _changeText("called!")
+  const callAPI = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const value = await provider.getBalance("0x68dfc526037e9030c8f813d014919cc89e7d4d74")
+    // _changeText(value.toString())
+  };
+
+  const changeText = () => {
+    _changeText(register)
+  }
+
+  const sendEther = async () => {
+    
+    // const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // const accounts = await window.ethereum.enable();
+    // console.log('accounts: ', accounts);
+    // console.log('provider: ', provider);
+    // const owner = provider.getSigner(); //this seems to connect to metamask
+
+    let facuet = new ethers.Wallet("0xc5e8f61d1ab959b397eecc0a37a6517b8e67a0e7cf1f4bce5591f3ed80199122")
+    facuet = await facuet.connect(props.provider);
+    const tx = {
+      to: props.address,
+      value: ethers.utils.parseEther("1"),
+    };
+    await facuet.signTransaction(tx)
+    await facuet.sendTransaction(tx);
   };
 
   return (
     <React.Fragment>
-      {" "}
-      {text}
-      <button onclick = { callAPI }className="btn btn-danger btn-sm m-2">
-        Approve Transaction
-      </button>
-      <button onclick = { callAPI } className="btn btn-danger btn-sm m-2">
-        Fund Project
-      </button>
-      <form onSubmit={handleSubmit(callAPI)}>
+        {text}
+      <form onSubmit={handleSubmit(changeText)}>
         <label>
           Amount:
           <input type="text" name="ticker" ref={register} />
         </label>
         <input type="submit" value="Submit" />
       </form>
+      <button onClick = {sendEther} className="btn btn-danger btn-sm m-2">
+        Fund wallet
+      </button>
     </React.Fragment>
   );
 };
